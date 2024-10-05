ebbb9e9f369e7c09a69b7c1796fe6f1992868257
tderamus <deramust@gmail.com>
2024-10-05 13:31:54 -0500

    fixed create author api and create author form to handle form data submission to database and render author cards

---
+++ src/app/author/[userId]/page.js
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getAuthors } from '../../../api/authorData';
+import AuthorCard from '../../../components/AuthorCard';
+
+/* eslint-disable react-hooks/exhaustive-deps */
+function ShowAuthors({ params }) {
+  // Set a state for authors
+  const [authors, setAuthors] = useState([]);
+
+  // create a function that makes the API call to get all the authors
+  const getAllTheAuthors = () => {
+    getAuthors(params.uid).then(setAuthors);
+  };
+
+  // make the call to the API to get all the autors on component render
+  useEffect(() => {
+    getAllTheAuthors();
+  }, []);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/author/new" passHref>
+        <Button type="button">Add An Author</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {/* map over authors here using AuthorCard component */}
+        {authors.map((author) => (
+          <AuthorCard key={author.firebaseKey} authorObj={author} onUpdate={getAllTheAuthors} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+ShowAuthors.propTypes = {
+  params: PropTypes.string.isRequired,
+};
+
+export default ShowAuthors;
---
+++ src/app/author/new/page.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuthorForm from '@/components/forms/AuthorForm';
+
+// create a reusable form to add/edit author and render in this view
+
+export default function AddAuthor() {
+  return <AuthorForm />;
+}
